f6d2147e4b23f25468d228aa4b9cbf441f114e60
Sawwave <timanderson.um@gmail.com>
2023-03-02 15:18:16 -0700

    Update Havac for better smart ptr usage

--- host/Havac.cpp
+++ host/Havac.cpp
@@ -1,32 +1,33 @@
 #include "Havac.hpp"
 #include "../PhmmReprojection/PhmmReprojection.h"
-#include <exception>
-#include <stdexcept>
 #include "exceptions/FileReadException.hpp"
+#include "phmm/PhmmPreprocessor.hpp"
+#include "sequence/SequencePreprocessor.hpp"
 
-Havac::Havac(const uint32_t deviceIndex = 0)
-  :deviceIndex(deviceIndex) {
-  this->hwClient = std::shared_ptr<HavacHwClient>(new HavacHwClient(this->havacXclbinFileSrc, this->havacKernelName, deviceIndex));
-  this->fastaVector = new FastaVector;
-  this->p7HmmList = new P7HmmList;
+#include <stdexcept>
+#include <exception>
 
-  enum FastaVectorReturnCode rc = fastaVectorInit(&this->fastaVector);
-  if (rc == FASTA_VECTOR_INIT_FAIL) {
+Havac::Havac(const uint32_t deviceIndex)
+  :deviceIndex(deviceIndex){
+  this->hwClient = shared_ptr<HavacHwClient>(new HavacHwClient(this->havacXclbinFileSrc, this->havacKernelName, deviceIndex));
+  this->fastaVector = shared_ptr<FastaVector>(new FastaVector);
+  this->p7HmmList = shared_ptr<P7HmmList>(new P7HmmList);
+  //here, 
+  enum FastaVectorReturnCode rc = fastaVectorInit(this->fastaVector.get());
+  if (rc == FASTA_VECTOR_ALLOCATION_FAIL) {
     throw std::bad_alloc();
   }
 }
 Havac::~Havac() {
-  fastaVectorDealloc(this->fastaVector);
-  p7HmmListDealloc(this->p7HmmList);
-  delete (this->fastaVector);
-  delete (this->p7HmmList);
+  fastaVectorDealloc(this->fastaVector.get());
+  p7HmmListDealloc(this->p7HmmList.get());
 
 }
 
 
 void Havac::loadPhmm(const std::string phmmSrc, const float desiredPValue) {
 
-  enum P7HmmReturnCode rc = readP7Hmm(phmmSrc.c_str(), this->p7HmmList);
+  enum P7HmmReturnCode rc = readP7Hmm(phmmSrc.c_str(), this->p7HmmList.get());
   if (rc == p7HmmAllocationFailure) {
     throw std::bad_alloc();
   }
@@ -34,91 +35,71 @@ void Havac::loadPhmm(const std::string phmmSrc, const float desiredPValue) {
     throw std::runtime_error("Phmm file was not formatted correctly.");
   }
 
-  std::shared_ptr<PhmmPreprocessor> phmmPreprocessor = 
-    std::shared_ptr<PhmmPreprocessor>(new PhmmPreprocessor(this->p7HmmList, desiredPValue));
-  const int8_t* phmmDataPtr = phmmPreprocessor->getProcessedPhmmData().get();
-  const uint32_t phmmLengthInBytes = phmmPreprocessor->getPhmmLengthInBytes();
+  shared_ptr<PhmmPreprocessor> phmmPreprocessor =
+    shared_ptr<PhmmPreprocessor>(new PhmmPreprocessor(this->p7HmmList, desiredPValue));
 
-  this->hwClient->writePhmm(phmmDataPtr, phmmLengthInBytes);
+  shared_ptr<vector<int8_t>> phmmDataPtr = phmmPreprocessor->getProcessedPhmmData();
+
+  this->hwClient->writePhmm(phmmDataPtr);
   this->phmmLoadedToDevice = true;
 }
 
 void Havac::loadSequence(const std::string fastaSrc) {
-  enum FastaVectorReturnCode rc = fastaVectorReadFasta(fastaSrc, this->fastaVector);
+  enum FastaVectorReturnCode rc = fastaVectorReadFasta(fastaSrc.c_str(), this->fastaVector.get());
   if (rc == FASTA_VECTOR_ALLOCATION_FAIL) {
     throw std::bad_alloc();
   }
   else if (rc == FASTA_VECTOR_FILE_OPEN_FAIL) {
-    throw FileReadException("Could not open fasta file for reading.");
+    throw "Could not open fasta file for reading.";
   }
   else if (rc == FASTA_VECTOR_FILE_READ_FAIL) {
-    throw FileReadException("Error while reading from the opened fasta file.");
+    throw "Error while reading from the opened fasta file.";
   }
 
-  std::shared_ptr<SequencePreprocessor> sequencePreprocessor = 
-    std::shared_ptr<SequencePreprocessor>(new SequencePreprocessor(this->fastaVector));
+  shared_ptr<SequencePreprocessor> sequencePreprocessor =
+    shared_ptr<SequencePreprocessor>(new SequencePreprocessor(this->fastaVector.get()));
 
 
-  uint8_t* sequenceAsBytes = sequencePreprocessor->getCompressedSequenceBuffer.get();
-  uint32_t sequenceLengthInBytes = sequencePreprocessor->getCompressedSquenceLengthInBytes()
-  hwClient->writeSequence(sequenceAsBytes, sequenceLengthInBytes);
-
+  hwClient->writeSequence(sequencePreprocessor->getCompressedSequenceBuffer());
   this->sequenceLoadedToDevice = true;
-
 }
 
 
-void Havac::runHardwareClient(){
+void Havac::runHardwareClient() {
   this->runHardwareClientAsync();
   this->waitHardwareClientAsync();
 }
 
 void Havac::runHardwareClientAsync() {
-  if(!this->phmmLoadedToDevice){
+  if (!this->phmmLoadedToDevice) {
     throw std::logic_error("Phmm was not loaded to device before hardware was requested to run.");
   }
   if (!this->sequenceLoadedToDevice) {
     throw std::logic_error("Sequence was not loaded to device before hardware was requested to run.");
   }
+
   this->hwClient->invokeHavacSsvAsync();
 }
 
-void Havac::waitHardwareClientAsync(){
+void Havac::waitHardwareClientAsync() {
   this->hwClient->waitForHavacSsvAsync();
 }
 
-void Havac::abortHardwareClient(){
+void Havac::abortHardwareClient() {
   this->hwClient->abort();
 }
 
-std::shared_ptr<std::vector<HavacHitLocation>> getHitsFromFinishedRun(){
-  std::shared_ptr<HitVerifier> hitVerifier = 
-  std::shared_ptr<HitVerifier>(new HitVerifier(this->fastaVector, this->p7HmmmList));
-  hitVerifer->verify();
+shared_ptr<vector<VerifiedHit>> Havac::getHitsFromFinishedRun() {
+  shared_ptr<HitVerifier> hitVerifier(new HitVerifier(this->fastaVector, this->p7HmmList));
+  shared_ptr<vector<HardwareHitReport>> unverifiedHitList = this->hwClient->getHitReportList();
+  return hitVerifier->verify(unverifiedHitList);
+}
+
+
+ert_cmd_state Havac::currentHardwareState() {
+  return hwClient->getHwState();
 }
 
 bool Havac::isRunning() {
-  //not yet implemented
+  return this->currentHardwareState() == ERT_CMD_STATE_RUNNING;
 }
-
-
-// void Havac::generateProjectedPhmmMatchScores(const float pValue = 0.05f) {
-//   //determine how long the projectedScoresArray needs to be.
-//   uint32_t projectedScoresLength = 0;
-//   for (uint32_t phmmIndex = 0; phmmIndex < this->p7HmmList->count; phmmIndex++) {
-//     projectedScoresLength += this->phmmList->phmms[phmmIndex].header.modelLength * 4;
-//   }
-
-//   try {
-//     projectedPhmmMatchScores = make_shared<int8_t[]>(projectedScoresLength);
-//   }
-//   (const std::bad_alloc & e) {
-//     std::cerr << "ERROR: could not allocate memory for reprojected phmm scores.\n" << e.what() << std::endl;
-//   }
-//   uint32_t phmmOffset = 0
-//     for (uint32_t phmmIndex = 0; phmmIndex < this->phmmList->count; phmmIndex++) {
-//       int8_t* projectedPhmmScorePtr = &projectedPhmmMatchScores[phmmOffset * 4];
-//       p7HmmProjectForThreshold256(&this->phmmList->phmms[phmmIndex], pValue, projectedPhmmScorePtr);
-//       phmmOffset += this->phmmList->phmms[phmmIndex].header.modelLength;
-//     }
-// }
--- host/Havac.hpp
+++ host/Havac.hpp
@@ -2,50 +2,81 @@
 #define HAVAC_HOST_HPP
 
 #include "HavacHwClient.hpp"
-#include <string>
-#include <memory>
-
+#include "HitVerifier.hpp"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+#include "xrt/xrt_bo.h"
 extern "C"{
   #include <FastaVector.h>
   #include <p7HmmReader.h>
 }
+#include <string>
+#include <memory>
 
 using std::shared_ptr;
-using std::unique_ptr;
+using std::vector;
 
-struct HavacHitLocation {
-  uint32_t sequenceIndex;
-  uint32_t hitLocationInSequence;
-  uint32_t phmmIndex;
-  uint32_t hitLocationInPhmm;
-};
 
 class Havac {
 public:
-  Havac(const uint32_t deviceIndex = 0, const float pValue = 0.05f);
+  /// @brief creates a HAVAC control object, which gives a simple interface to the hardware coprocessor
+  /// @param deviceIndex index of the FPGA device, this should be 0 unless multiple cards are installed.
+  ///   If this doesn't work, try iterating different deviceIndexes. Unfortunately, the XRT docs doesn't
+  ///   speficy anything further, sorry.
+  Havac(const uint32_t deviceIndex = 0);
   Havac(Havac&& havac) = delete;
   Havac(Havac& havac) = delete;
   ~Havac();
 
+  /// @brief load the sequence data from the given fasta file source to the hardware client
+  /// @param fastaSrc file location to load
   void loadSequence(const std::string fastaSrc);
-  void loadPhmm(const std::string phmmSrc, const float desiredPValue));
+
+  /// @brief load the phmm from the given file src to the hardware client
+  /// @param phmmSrc location of file to read
+  /// @param desiredPValue p value that would cause a hit to be registered
+  void loadPhmm(const std::string phmmSrc, const float desiredPValue = 0.05f);
+
+  /// @brief run the HAVAC hardware client synchronously, and return when finished
   void runHardwareClient();
+
+  /// @brief run the HAVAC hardware client, and return immediately. 
   void runHardwareClientAsync();
+
+  /// @brief while HAVAC client is running, wait until it has finished
   void waitHardwareClientAsync();
+
+  /// @brief abort the current run of HAVAC
   void abortHardwareClient();
-  std::vector<HavacHitLocation>& getHitsFromFinishedRun();
+
+  /// @brief retrieve the hits from a finished HAVAC run
+  /// @return the list of hits, after having been verified via bounded reference SSV checks.
+  shared_ptr<vector<VerifiedHit>> getHitsFromFinishedRun();
+
+  /// @brief gets the current hardware state
+  /// @return state of the hardware, possible returns are:
+  ///   ERT_CMD_STATE_NEW = 1, 
+  ///   ERT_CMD_STATE_QUEUED = 2,
+  ///   ERT_CMD_STATE_RUNNING = 3,
+  ///   ERT_CMD_STATE_COMPLETED = 4,
+  ///   ERT_CMD_STATE_ERROR = 5,
+  ///   ERT_CMD_STATE_ABORT = 6,
+  ///   ERT_CMD_STATE_SUBMITTED = 7,
+  ///   ERT_CMD_STATEIMEOUT = 8,
+  ///   ERT_CMD_STATE_NORESPONSE = 9
+  enum ert_cmd_state currentHardwareState();
 
 private:
   shared_ptr<HavacHwClient> hwClient;
-  FastaVector *fastaVector;
-  P7HmmList *p7HmmList;
+  shared_ptr<FastaVector> fastaVector;
+  shared_ptr<P7HmmList> p7HmmList;
   shared_ptr<int8_t[]> projectedPhmmMatchScores;
   uint32_t deviceIndex;
   bool phmmLoadedToDevice = false;
   bool sequenceLoadedToDevice = false;
 
-  static const std::string havacXclbinFileSrc = "impl/havac.xclbin";
-  static const std::string havacKernelName = "havacKernel";
+  const std::string havacXclbinFileSrc = "impl/havac.xclbin";
+  const std::string havacKernelName = "havacKernel";
 
   bool isRunning();
   void generateProjectedPhmmMatchScores(const float pValue = 0.05f);
