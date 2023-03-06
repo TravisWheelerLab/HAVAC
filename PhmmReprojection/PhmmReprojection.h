ce4b5a7d6f400712e7eb2ea633cab6e2602d0c02
Sawwave <timanderson.um@gmail.com>
2023-03-06 16:58:16 -0700

    add documentation for reprojection code

--- PhmmReprojection/PhmmReprojection.h
+++ PhmmReprojection/PhmmReprojection.h
@@ -11,10 +11,14 @@ extern "C" {
 ///   will multiply by -1, thus creating positive scores for good matches, and negative scores for bad matches.
 /// @param phmm pointer to the phmm to project. these score multipliers are on a per-model basis
 /// @param desiredPValue The p value required to generate a score of 256, thus generating a hit.
-/// @return 
+/// @return  score multipler needed for each score in the given phmm.
 float generateScoreMultiplierForPhmmScore(const struct P7Hmm* const phmm, const float desiredPValue);
 
 
+/// @brief projects a score as given in a phmm file into a score for use with a constant 256 hit threshold.
+/// @param phmmScore score from the phmm file. this will be a negative log-odds score.
+/// @param scoreMultiplier multiplier found via the generateScoreMultiplierForPhmmScore() function.
+/// @return projected score in int8_t space.
 float projectPhmmScoreWithMultiplier(const float phmmScore, const float scoreMultiplier);
 
 /// @brief projects the scores inside the given phmm such that an accumulated score of 256 represents a hit at
