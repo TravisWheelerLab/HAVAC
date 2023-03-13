fd2f886aaceaf6c79445861ec34d0aecf3065f70
Sawwave <timanderson.um@gmail.com>
2023-03-13 14:53:16 -0600

    update to only verify SSV on diags  that could hit

--- host/HitVerifier.cpp
+++ host/HitVerifier.cpp
@@ -11,16 +11,16 @@
 
 HitVerifier::HitVerifier(shared_ptr<FastaVector> fastaVector, shared_ptr<P7HmmList> phmmList)
   :fastaVector(fastaVector),
-  phmmList(phmmList){
+  phmmList(phmmList) {
   this->ssvCellScores = std::make_shared<vector<float>>(SSV_PHMM_VERIFICATION_RANGE);
 }
 
 
 
-shared_ptr<vector<VerifiedHit>> HitVerifier::verify(shared_ptr<vector<HardwareHitReport>> hits) {
+shared_ptr<vector<VerifiedHit>> HitVerifier::verify(shared_ptr<vector<HardwareHitReport>> hits, const float desiredPValue) {
   shared_ptr<vector<VerifiedHit>> verifiedHitList = std::make_shared<vector<VerifiedHit>>();
   for (const auto& hit : *hits) {
-    verifyHit(hit, verifiedHitList);
+    verifyHit(hit, verifiedHitList, desiredPValue);
   }
 
   return verifiedHitList;
@@ -64,17 +64,12 @@ std::tuple<uint32_t, uint32_t> HitVerifier::getPhmmIndexFromPosition(const Hardw
 }
 
 //can throw std::domain_error
-void HitVerifier::verifyHit(const HardwareHitReport& hardwareHitReport, shared_ptr<vector<VerifiedHit>> verifiedHitList) {
+void HitVerifier::verifyHit(const HardwareHitReport& hardwareHitReport, shared_ptr<vector<VerifiedHit>> verifiedHitList, const float desiredPValue) {
 
   //find which phmm the hit was found in, and the local position of the hit.
   uint32_t phmmIndex;
   uint32_t localPhmmHitPosition;
   std::tie(phmmIndex, localPhmmHitPosition) = getPhmmIndexFromPosition(hardwareHitReport);
-
-  uint32_t thisPhmmLength = this->phmmList->phmms[phmmIndex].header.modelLength;
-  uint32_t localSsvPhmmStartPosition = std::max(0L, (int64_t)localPhmmHitPosition - (int64_t)SSV_VERIFICATION_PRIOR_FLANK_RANGE);
-  uint32_t localSsvPhmmEndPosition = std::min(thisPhmmLength, localPhmmHitPosition + SSV_VERIFICATION_POST_FLANK_RANGE);
-  uint32_t localPhmmSsvLength = localSsvPhmmEndPosition - localSsvPhmmStartPosition;
 
   //now we have enough info to get the phmm data needed for the local ssv verification. Now, 
   //figure out which sequence segments might be in the range. 
@@ -98,12 +93,11 @@ void HitVerifier::verifyHit(const HardwareHitReport& hardwareHitReport, shared_p
     uint32_t thisSequenceEndPosition = this->fastaVector->metadata.data[sequenceIndex].sequenceEndPosition;
 
     //if there is overlap, it's worth checking for an actual hit.
-    bool sequenceOverlapsWithPossibleHit = 
-    (thisSequenceStartPosition <= sequencePossibleEndPosition) &&
+    bool sequenceOverlapsWithPossibleHit =
+      (thisSequenceStartPosition <= sequencePossibleEndPosition) &&
       (thisSequenceEndPosition > sequencePossibleStartPosition);
-    
-    if (sequenceOverlapsWithPossibleHit) {
 
+    if (sequenceOverlapsWithPossibleHit) {
       //these sequences overlap, the hit might be in this sequence. 
       //the phmm will need local coordinates, but the sequence will need global coordinants.
       //keep the ssv range in the range of the actual sequence.
@@ -111,56 +105,61 @@ void HitVerifier::verifyHit(const HardwareHitReport& hardwareHitReport, shared_p
       //ssvSequenceRangeEnd is exclusive (this value may be out of bounds)
       const uint32_t ssvSequenceRangeEnd = std::min(thisSequenceEndPosition, sequencePossibleEndPosition);
 
-      this->verifyWithReferenceSsv(phmmIndex, sequenceIndex, localSsvPhmmStartPosition, localPhmmSsvLength, ssvSequenceRangeBegin, ssvSequenceRangeEnd);
+      this->verifyWithReferenceSsv(phmmIndex, sequenceIndex, localPhmmHitPosition, 
+        ssvSequenceRangeBegin, ssvSequenceRangeEnd, desiredPValue, verifiedHitList);
     }
   }
 }
 
-//TODO: debug-check this, and make sure the phmm scaling is right, we might have to rescale
-void HitVerifier::verifyWithReferenceSsv(const uint32_t hitLocatedInPhmmNumber, const uint32_t sequenceNumber, const uint32_t localSsvPhmmStartPosition,
-  const uint32_t localPhmmSsvLength, const uint32_t ssvSequenceRangeBegin, const uint32_t ssvSequenceRangeEnd) {
-
-  const float ssvThresholdScore = 256.0f; //the scores in the PHMM struct are prescaled for this 
-
-  const uint32_t ssvSequenceRangeLength = ssvSequenceRangeEnd - ssvSequenceRangeBegin;
-  //reinitialize the ssvCellScores array
-  std::memset(this->ssvCellScores.get()->data(), 0, sizeof(this->ssvCellScores));
-
-  uint8_t phmmCardinality = p7HmmGetAlphabetCardinality(&this->phmmList->phmms[hitLocatedInPhmmNumber]);
-  const float* phmmRange = &this->phmmList->phmms[hitLocatedInPhmmNumber].model.matchEmissionScores[localSsvPhmmStartPosition * phmmCardinality];
-
-  for (uint32_t sequencePosition = ssvSequenceRangeBegin; sequencePosition < ssvSequenceRangeEnd; sequencePosition++) {
-    char sequenceSymbol = this->fastaVector->sequence.charData[sequencePosition];
-    uint8_t sequenceSymbolAsIndex;
-    switch (sequenceSymbol) {
-    case 'a': case 'A': sequenceSymbolAsIndex = 0; break;
-    case 'c': case 'C': sequenceSymbolAsIndex = 1; break;
-    case 'g': case 'G': sequenceSymbolAsIndex = 2; break;
-    default:            sequenceSymbolAsIndex = 3;
-    }
-
-    auto cellScores = *this->ssvCellScores;
-    for (uint32_t phmmPosition = localPhmmSsvLength - 1; phmmPosition > 0; phmmPosition--) {
-      float matchScore = phmmRange[(phmmPosition * phmmCardinality) + sequenceSymbolAsIndex];
-      // const uint32_t positionInSsvCellScores = sequencePosition - ssvSequenceRangeBegin;
-      cellScores[phmmPosition] = cellScores[phmmPosition - 1] + matchScore;
-    }
-    //now compute the first cell
-    float matchScore = phmmRange[sequenceSymbolAsIndex];
-    cellScores[0] = matchScore;
 
 
-    for (uint32_t ssvCell = 0; ssvCell < localPhmmSsvLength; ssvCell++) {
-      if (cellScores[ssvCell] >= ssvThresholdScore) {
+void HitVerifier::verifyWithReferenceSsv(const uint32_t hitLocatedInPhmmNumber, const uint32_t sequenceNumber,
+  const uint32_t hitOccurredAtPhmmIndex, const uint32_t possibleSequenceIndexStart, const uint32_t possibleSequenceIndexEnd,
+  const float desiredPValue, shared_ptr<vector<VerifiedHit>> verifiedHitList) {
 
-        std::vector<VerifiedHit> verifiedHitLocations;
-        uint64_t sequencePosition = ssvSequenceRangeBegin + ssvCell;
-        uint32_t sequenceIndex = sequenceNumber;
-        uint32_t phmmPosition = localPhmmSsvLength + phmmPosition;
-        uint32_t phmmIndex = hitLocatedInPhmmNumber;
-        VerifiedHit verifiedHit(sequencePosition, sequenceIndex, phmmPosition, phmmIndex);
-        verifiedHitLocations.push_back(verifiedHit);
+  uint_fast16_t SSV_THRESHOLD = 256;
+  P7Hmm* phmmPointer = &this->phmmList->phmms[hitLocatedInPhmmNumber];
+  float ssvScoreMultiplier = generateScoreMultiplierForPhmmScore(phmmPointer, desiredPValue);
+
+  uint8_t phmmCardinality = p7HmmGetAlphabetCardinality(phmmPointer);
+  const float* phmmRange = &phmmPointer->model.matchEmissionScores[hitOccurredAtPhmmIndex * phmmCardinality];
+
+  //iterate over each cell that could have caused the hit
+  for (uint32_t sequenceConsiderationIndex = possibleSequenceIndexStart; sequenceConsiderationIndex < possibleSequenceIndexEnd;
+    sequenceConsiderationIndex++) {
+    //the length of the diagonal to accumulate score on. this could be shorter if that length would go past the start of the phmm or sequence  
+    const uint32_t MAX_SSV_WALKBACK_DISTANCE = 64;
+    uint32_t diagonalWalkbackLength = std::min({ MAX_SSV_WALKBACK_DISTANCE, hitOccurredAtPhmmIndex, possibleSequenceIndexStart });
+
+    int_fast16_t accumulatedScore = 0;
+    bool foundThresholdHitOnDiagonal = false;
+    for (uint32_t ssvWalkbackIndex = 0; ssvWalkbackIndex < diagonalWalkbackLength; ssvWalkbackIndex++) {
+      const uint32_t walkbackPhmmIndex = hitOccurredAtPhmmIndex - ssvWalkbackIndex;
+      float* phmmVectorAsFloat = &phmmPointer->model.matchEmissionScores[walkbackPhmmIndex * phmmCardinality];
+
+      const uint32_t walkbackSequenceIndex = sequenceConsiderationIndex - ssvWalkbackIndex;
+      char sequenceSymbol = this->fastaVector->sequence.charData[walkbackSequenceIndex];
+      uint8_t sequenceSymbolAsIndex;
+      switch (sequenceSymbol) {
+      case 'a': case 'A': sequenceSymbolAsIndex = 0; break;
+      case 'c': case 'C': sequenceSymbolAsIndex = 1; break;
+      case 'g': case 'G': sequenceSymbolAsIndex = 2; break;
+      default:            sequenceSymbolAsIndex = 3;
       }
+
+      float unprojectedMatchScore = phmmVectorAsFloat[sequenceSymbolAsIndex];
+      float projectedPhmmMatchScore = projectPhmmScoreWithMultiplier(unprojectedMatchScore, ssvScoreMultiplier);
+      int_fast16_t matchScoreAsInt = projectedPhmmMatchScore;
+      accumulatedScore += matchScoreAsInt;
+      //godbold suggests that (on x86 GCC) if the threshold check comes before the saturation check, we won't have any branches
+      foundThresholdHitOnDiagonal |= accumulatedScore > SSV_THRESHOLD;
+      if (accumulatedScore < 0) {
+        accumulatedScore = 0;
+      }
+    }
+    if (foundThresholdHitOnDiagonal) {
+      VerifiedHit hit(sequenceConsiderationIndex, sequenceNumber, hitOccurredAtPhmmIndex, hitLocatedInPhmmNumber);
+      verifiedHitList->push_back(hit);
     }
   }
 }
