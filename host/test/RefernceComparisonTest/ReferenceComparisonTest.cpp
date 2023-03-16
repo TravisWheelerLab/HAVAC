62d2ffeab5df7c7823cef6fca776b90069908695
Sawwave <timanderson.um@gmail.com>
2023-03-16 17:51:09 -0600

    add module to verify hw returns hits that are equivalent to sw impl

---
+++ host/test/RefernceComparisonTest/ReferenceComparisonTest.cpp
@@ -0,0 +1,89 @@
+#include "ReferenceComparisonTest.hpp"
+#include "../Havac.hpp"
+#include "Ssv.hpp"
+#include "../../test/generator/hmmSeqGenerator.h"
+#include "../../device/PublicDefines.h"
+#include <memory>
+
+
+using std::vector;
+using std::make_shared;
+using std::shared_ptr;
+
+const char* phmmFileSrc = "refTest.hmm";
+const char* seqFileSrc = "refTest.fasta";
+const float desiredPValue = 0.05f;
+const uint32_t sequenceLength = NUM_CELL_PROCESSORS * 5;
+
+int main(int argc, char** argv) {
+  //run the hardware
+  shared_ptr<Havac> havac = make_shared<Havac>(0);
+  generateRandomHmmSeqPairToFiles(sequenceLength, seqFileSrc, phmmFileSrc);
+  struct HmmSeqPair generateRandomHmmSeqPair(const uint32_t seqLength);
+  havac->loadPhmm(phmmFileSrc, desiredPValue);
+  havac->loadSequence(seqFileSrc);
+  havac->runHardwareClient();
+  shared_ptr<vector<VerifiedHit>> hardwareHits = havac->getHitsFromFinishedRun();
+
+
+  //run the software impl
+  //load the fasta and phmm from the files
+  FastaVector fastaVector;
+  P7HmmList phmmList;
+  FastaVectorReturnCode fastaRc = fastaVectorReadFasta(seqFileSrc, &fastaVector);
+  P7HmmReturnCode phmmRc = readP7Hmm(phmmFileSrc, &phmmList);
+
+  shared_ptr<vector<ReferenceSsvHit>> softwareHits = HitsFromSsv(&fastaVector,
+    &phmmList, desiredPValue);
+
+
+  compareHardwareSoftwareHits(hardwareHits, softwareHits);
+}
+
+
+
+void compareHardwareSoftwareHits(shared_ptr<vector<VerifiedHit>> hardwareHits, shared_ptr<vector<ReferenceSsvHit>> softwareHits) {
+  {
+    bool foundAllHwHits = true;
+    for (VerifiedHit& hwHit : *hardwareHits) {
+      bool hwHitFoundinSwHits = false;
+
+      for (ReferenceSsvHit& swHit : *softwareHits) {
+        if (hwHit.phmmIndex == swHit.phmmNumber &&
+          hwHit.phmmPosition == swHit.phmmPosition &&
+          hwHit.sequenceIndex == swHit.sequenceNumber &&
+          hwHit.sequencePosition == swHit.sequencePosition) {
+          hwHitFoundinSwHits = true;
+        }
+      }
+      if (!hwHitFoundinSwHits) {
+        foundAllHwHits = false;
+        std::cout << "error: hit not found in software hits: " << hwHit.toString() << std::endl;
+      }
+    }
+    if (foundAllHwHits) {
+      std::cout << "found all " << hardwareHits->size() << "hardware hits" << std::endl;
+    }
+  }
+  {
+    bool foundAllSwHits;
+    for (ReferenceSsvHit& swHit : *softwareHits) {
+      bool swHitFoundInHwHits = false;
+      for (VerifiedHit& hwHit : *hardwareHits) {
+        if (hwHit.phmmIndex == swHit.phmmNumber &&
+          hwHit.phmmPosition == swHit.phmmPosition &&
+          hwHit.sequenceIndex == swHit.sequenceNumber &&
+          hwHit.sequencePosition == swHit.sequencePosition) {
+          swHitFoundInHwHits = true;
+        }
+      }
+      if (!swHitFoundInHwHits) {
+        foundAllSwHits = false;
+        std::cout << "error: hit not found in software hits: " << swHit.toString() << std::endl;
+      }
+    }
+    if (foundAllSwHits) {
+      std::cout << "found all " << hardwareHits->size() << "hardware hits" << std::endl;
+    }
+  }
+}
---
+++ host/test/RefernceComparisonTest/ReferenceComparisonTest.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+
+void compareHardwareSoftwareHits(shared_ptr<vector<VerifiedHit>> hardwareHits, shared_ptr<vector<ReferenceSsvHit>> softwareHits);
