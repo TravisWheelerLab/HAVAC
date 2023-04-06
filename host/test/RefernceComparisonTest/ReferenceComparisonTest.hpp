75465a218c6a60f322a3d770564d61f3202a5951
Sawwave <timanderson.um@gmail.com>
2023-04-06 17:33:09 -0600

    add includes for utility code, move usings to header

--- host/test/RefernceComparisonTest/ReferenceComparisonTest.hpp
+++ host/test/RefernceComparisonTest/ReferenceComparisonTest.hpp
@@ -1,4 +1,12 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+#include "Ssv.hpp"
+#include "../../types/VerifiedHit.hpp"
+
+using std::shared_ptr;
+using std::vector;
+using std::make_shared;
 
 void compareHardwareSoftwareHits(shared_ptr<vector<VerifiedHit>> hardwareHits, shared_ptr<vector<ReferenceSsvHit>> softwareHits);
