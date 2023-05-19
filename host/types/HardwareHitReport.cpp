39a733669a0bd26c20ab0abf46b0c4c42572f031
Sawwave <timanderson.um@gmail.com>
2023-05-19 15:55:49 -0600

    change hardware hit report to support wider ranges of # groups

--- host/types/HardwareHitReport.cpp
+++ host/types/HardwareHitReport.cpp
@@ -1,17 +1,20 @@
 #include "HardwareHitReport.hpp"
-
+#include <sstream>
+#include <bitset>
+#include <string.h>
+#include "../../device/PublicDefines.h"
 
 HardwareHitReport::HardwareHitReport()
   :sequencePassIndex(0),
-  sequenceGroupIndex(0),
+  groupHitBits({ 0 }),
   phmmPosition(0) {
 }
 
 
-HardwareHitReport::HardwareHitReport(uint32_t sequencePassIndex, uint32_t sequenceGroupIndex,
+HardwareHitReport::HardwareHitReport(uint32_t sequencePassIndex, uint8_t groupHitBits[GROUP_HIT_BITS_NUM_BYTES],
   uint32_t phmmPosition)
   :sequencePassIndex(sequencePassIndex),
-  sequenceGroupIndex(sequenceGroupIndex),
   phmmPosition(phmmPosition) {
+  memcpy(this->groupHitBits, groupHitBits, GROUP_HIT_BITS_NUM_BYTES);
 
 }
--- host/types/HardwareHitReport.hpp
+++ host/types/HardwareHitReport.hpp
@@ -2,15 +2,23 @@
 #define HAVAC_HARDWARE_HIT_REPORT_HPP
 
 #include <cstdint>
+#include <string>
+#include <device/PublicDefines.h>
 
-class HardwareHitReport{
-  public:
-    HardwareHitReport();
-    HardwareHitReport(uint32_t sequencePassIndex, uint32_t sequenceGroupIndex,
-      uint32_t phmmPosition);
-    uint32_t sequencePassIndex;
-    uint32_t sequenceGroupIndex;
-    uint32_t phmmPosition;
+#define GROUP_HIT_BITS_NUM_BYTES (NUM_CELL_GROUPS /8)
+
+class HardwareHitReport {
+public:
+  HardwareHitReport();
+  HardwareHitReport(uint32_t sequencePassIndex, uint8_t groupHitBits[GROUP_HIT_BITS_NUM_BYTES],
+    uint32_t phmmPosition);
+
+  static_assert(NUM_CELL_GROUPS % 8 == 0, "num groups is required to be a multiple of 8, but was not.");
+
+  uint32_t phmmPosition;
+  uint32_t sequencePassIndex;
+  uint8_t groupHitBits[GROUP_HIT_BITS_NUM_BYTES];
+  std::string toString();
 };
 
 #endif
