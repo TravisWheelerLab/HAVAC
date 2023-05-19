e9bf312b9569159798144b66a468cefaefd1560e
Sawwave <timanderson.um@gmail.com>
2023-05-19 16:00:37 -0600

    update allocation sizes for client buffers

--- host/HavacHwClient.hpp
+++ host/HavacHwClient.hpp
@@ -90,9 +90,11 @@ protected:
   uint32_t phmmLengthInVectors;
   uint32_t numHits;
 
-  static const uint64_t sequenceAllocationSizeInBytes = 512UL * 1024UL * 1024UL * 8UL;  //4.0GiB
-  static const uint32_t phmmAllocationSizeInBytes = 512 * 1024 * 1024;         //0.5GiB
-  static const uint32_t hitReportAllocationSizeInBytes = 512 * 1024 * 1024;     //0.5GiB
+  // static const uint64_t sequenceAllocationSizeInBytes = 512UL * 1024UL * 1024UL * 8UL;  //4.0GiB
+  static const uint64_t sequenceAllocationSizeInBytes = 3UL * 1024UL * 1024UL * 1024UL;  //3GiB
+  static const uint32_t phmmAllocationSizeInBytes = 1UL * 1024UL * 1024UL;         //1MiB
+  //HAVAC supports 1024*1024 positions in the phmm, so the allocated size should be 4x that (1byte/symbol/position)
+  static const uint32_t hitReportAllocationSizeInBytes = 4UL * 1024UL * 1024UL;     //4MiB
 
   /// @brief attempts to generate the kernel for the given .xclbin file and kernel name
   /// @param xclbinFileSrc source where the .xclbin file is located
