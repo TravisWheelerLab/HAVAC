0ec14f3e68873a1203b2bf51641c1eb94bba3293
Sawwave <timanderson.um@gmail.com>
2023-09-07 15:00:50 -0600

    update for formatting

--- host/phmm/PhmmPreprocessor.cpp
+++ host/phmm/PhmmPreprocessor.cpp
@@ -1,10 +1,12 @@
 #include "PhmmPreprocessor.hpp"
-
 #include "../../PhmmReprojection/PhmmReprojection.h"
+
+#include <iostream>
+#include <fstream>
 
 #define BYTES_PER_PHMM_VECTOR 4
 
-PhmmPreprocessor::PhmmPreprocessor(P7HmmList *phmmList, const float desiredPvalue) {
+PhmmPreprocessor::PhmmPreprocessor(P7HmmList* phmmList, const float desiredPvalue) {
   //concatenates the lengths of all vectors in the phmm file.
   this->phmmDataLengthInVectors = 0;
   for (uint32_t i = 0; i < phmmList->count; i++) {
@@ -17,13 +19,14 @@ PhmmPreprocessor::PhmmPreprocessor(P7HmmList *phmmList, const float desiredPvalu
   //individually projects each model such that a hit is generated on a score of 256,
   //and sets the data for the phmmData member variable.
   uint32_t currentIndexIntoProcessedPhmmData = 0;
-  for(uint32_t i = 0; i < phmmList->count; i++){
+  for (uint32_t i = 0; i < phmmList->count; i++) {
     int8_t* currentPointerIntoProjectedPhmmData = this->phmmData->data() + currentIndexIntoProcessedPhmmData;
     p7HmmProjectForThreshold256(&phmmList->phmms[i], desiredPvalue, currentPointerIntoProjectedPhmmData);
 
     const uint32_t thisModelLength = phmmList->phmms[i].header.modelLength;
     const uint32_t thisModelLengthInBytes = thisModelLength * BYTES_PER_PHMM_VECTOR;
     currentIndexIntoProcessedPhmmData += thisModelLengthInBytes;
+
   }
 }
 
@@ -31,10 +34,10 @@ shared_ptr<vector<int8_t>> PhmmPreprocessor::getProcessedPhmmData() {
   return this->phmmData;
 }
 
-uint32_t PhmmPreprocessor::getPhmmLengthInBytes(){
+uint32_t PhmmPreprocessor::getPhmmLengthInBytes() {
   return this->phmmDataLengthInBytes;
 }
 
-uint32_t PhmmPreprocessor::getPhmmListLengthInVectors(){
+uint32_t PhmmPreprocessor::getPhmmListLengthInVectors() {
   return this->phmmDataLengthInVectors;
 }
